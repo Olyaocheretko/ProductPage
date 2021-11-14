525c986e596d7e37c14119652ad8c7604163eac7
Olya Ocheretko <olyaocheretko@gmail.com>
2021-11-14 21:36:53 +0200

    Adding contact form values saving, favicon, small changes

--- js/script.js
+++ js/script.js
@@ -18,30 +18,53 @@ for (let index = 0; index < tabs_buttons.length; index++) {
   }
 }
 //Quantity==============================================================
-let quantityButtons = document.querySelectorAll('.quantity__element-button')
+const quantityButtons = document.querySelectorAll('.quantity__element-button')
 
 for (let index = 0; index < quantityButtons.length; index++) {
   let quantityButton = quantityButtons[index]
 
   quantityButton.onclick = function(event) {
-    let quantityInput = document.getElementById('quantity')
+    const quantityInput = document.getElementById('quantity')
     let value = quantityInput.value
-    let infoQuantityElement = document.getElementById('info_quantity')
-
+    const infoQuantityElement = document.getElementById('info_quantity')
     quantityButton.classList.contains('quantity__plus') ?  value++ : value--
     value = value < 1 ? 1 : value
     quantityInput.value = value
     infoQuantityElement.textContent = `${value}`
 
-    let priceElement = document.getElementById('price')
+    const priceElement = document.getElementById('price')
     let price = priceElement.textContent || priceElement.innerText
     price = +price
     let totalPrice = price * value
-
-    let totalPriceElement = document.getElementById('totalPrice')
-    let infoTotalPriceElement = document.getElementById('info_price')
+    const totalPriceElement = document.getElementById('totalPrice')
+    const infoTotalPriceElement = document.getElementById('info_price')
     totalPriceElement.textContent = `${totalPrice}`
     infoTotalPriceElement.textContent = `${totalPrice}`
+  }
+}
+//Contact info==========================================================
+const contactsButtons = document.querySelectorAll('.form__input')
+
+for (let index = 0; index < contactsButtons.length; index++) {
+  let contactsButton = contactsButtons[index]
+
+  contactsButton.oninput = function(event) {
+    let nameValue = document.getElementById('forUserName').value
+    let surnameValue = document.getElementById('forUserSurname').value
+    let emailValue = document.getElementById('forUserEmail').value
+    let phoneValue = document.getElementById('forUserPhone').value
+    let addressValue = document.getElementById('forUserAddress').value
+    const infoNameInput = document.getElementById('info_name')
+    const infoSurnameInput = document.getElementById('info_surname')
+    const infoEmailInput = document.getElementById('info_email')
+    const infoPhoneInput = document.getElementById('info_phone')
+    const infoAddressInput = document.getElementById('info_address')
+
+    infoNameInput.textContent = `${nameValue}`
+    infoSurnameInput.textContent = `${surnameValue}`
+    infoEmailInput.textContent = `${emailValue}`
+    infoPhoneInput.textContent = `${phoneValue}`
+    infoAddressInput.textContent = `${addressValue}`
   }
 }
 
