49327480898927926bb8faa3a25c5208b853c12b
Olya Ocheretko <olyaocheretko@gmail.com>
2021-12-28 14:21:09 +0200

    Add functions, change approach for input fields

--- js/script.js
+++ js/script.js
@@ -1,73 +1,54 @@
-//Tabs==================================================================
 const tabs = document.getElementById('tabs')
 const tabs_buttons = tabs.getElementsByClassName('_tabs-button')
 const tabs_blocks = tabs.getElementsByClassName('_tabs-block')
+const quantityChangingElements = document.getElementsByClassName('quantity__element-button')
+const contactsInputs = document.getElementsByClassName('form__input')
 
 for (let index = 0; index < tabs_buttons.length; index++) {
-  let tabs_button = tabs_buttons[index]
-
-  tabs_button.onclick = function(event) {
-    for (let index = 0; index < tabs_buttons.length; index++) {
-      let _tabs_button = tabs_buttons[index]
-
-      _tabs_button.classList.remove('_active')
-      tabs_blocks[index].classList.remove('_active')
-    }
-    this.classList.add('_active')
-    tabs_blocks[index].classList.add('_active')
+  tabs_buttons[index].onclick = function(event) {
+    makeTabActive(index)
   }
 }
-//Quantity==============================================================
-const quantityChangingElements = document.getElementsByClassName('quantity__element-button')
 
 for (let index = 0; index < quantityChangingElements.length; index++) {
-  let quantityChangingElement = quantityChangingElements[index]
-
-  quantityChangingElement.onclick = function(event) {
-    const quantityElement = document.getElementById('quantity')
-    let quantity = quantityElement.value
-
-    quantityChangingElement.classList.contains('quantity__plus') ?  quantity++ : quantity--
-    quantity = quantity <= 0 ? 0 : quantity
-    quantityElement.value = quantity
-
-    const quantity_output = document.getElementById('quantity_output')
-    quantity_output.textContent = `${quantity}`
-//price
-    const priceBase = document.getElementById('price_base')
-    let price = +priceBase.textContent || +priceBase.innerText
-    let totalPrice = price * quantity
-
-    const totalPrice_output1 = document.getElementById('totalPrice_output1')
-    const totalPrice_output2 = document.getElementById('totalPrice_output2')
-    totalPrice_output1.textContent = `${totalPrice}`
-    totalPrice_output2.textContent = `${totalPrice}`
+  quantityChangingElements[index].onclick = function(event) {
+    changeQuantity(this)
   }
 }
-//Contact info==========================================================
-const contactsInputs = document.getElementsByClassName('form__input')
 
 for (let index = 0; index < contactsInputs.length; index++) {
   let contactsInput = contactsInputs[index]
-
-  contactsInput.oninput = function(event) {
-    let nameValue = document.getElementById('forUserName').value
-    let surnameValue = document.getElementById('forUserSurname').value
-    let emailValue = document.getElementById('forUserEmail').value
-    let phoneValue = document.getElementById('forUserPhone').value
-    let addressValue = document.getElementById('forUserAddress').value
-
-    const infoNameInput = document.getElementById('info_name')
-    const infoSurnameInput = document.getElementById('info_surname')
-    const infoEmailInput = document.getElementById('info_email')
-    const infoPhoneInput = document.getElementById('info_phone')
-    const infoAddressInput = document.getElementById('info_address')
-
-    infoNameInput.textContent = `${nameValue}`
-    infoSurnameInput.textContent = `${surnameValue}`
-    infoEmailInput.textContent = `${emailValue}`
-    infoPhoneInput.textContent = `${phoneValue}`
-    infoAddressInput.textContent = `${addressValue}`
+  contactsInput.onchange = function(event) {
+    let infoFieldId = this.id.replace('form', 'info')
+    document.getElementById(infoFieldId).textContent = this.value
   }
 }
 
+function makeTabActive(tabIndex) {
+  for (let index = 0; index < tabs_buttons.length; index++) {
+    tabs_buttons[index].classList.remove('_active')
+    tabs_blocks[index].classList.remove('_active')
+  }
+  tabs_buttons[tabIndex].classList.add('_active')
+  tabs_blocks[tabIndex].classList.add('_active')
+}
+
+function changeQuantity(element) {
+  const quantityElement = document.getElementById('quantity')
+  let quantity = quantityElement.value
+  const quantity_output = document.getElementById('quantity_output')
+
+  element.classList.contains('quantity__plus') ?  quantity++ : quantity--
+  quantity = quantity <= 0 ? 0 : quantity
+  quantityElement.value = quantity
+  quantity_output.textContent = `${quantity}`
+
+  const priceBase = document.getElementById('price_base')
+  const totalPrice_output1 = document.getElementById('totalPrice_output1')
+  const totalPrice_output2 = document.getElementById('totalPrice_output2')
+
+  let price = +priceBase.textContent || +priceBase.innerText
+  let totalPrice = price * quantity
+  totalPrice_output1.textContent = `${totalPrice}`
+  totalPrice_output2.textContent = `${totalPrice}`
+}
